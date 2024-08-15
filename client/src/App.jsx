813771028c223edf9cac5781295b3468be8ad48b
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-15 19:41:57 +0545

    crated header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />

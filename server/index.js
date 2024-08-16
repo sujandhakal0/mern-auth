2481ed5f6ee86e871eabf05476cb14924e10ddc3
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-16 17:02:45 +0545

    add a error handler middleware

--- server/index.js
+++ server/index.js
@@ -6,14 +6,23 @@ import userRouter from "./routes/user.route.js";
 import Authrouter from "./routes/auth.route.js";
 
 const app = express();
-app.use(express.json())
-
+app.use(express.json());
 
 dotenv.config({ path: "./config/config.env" });
 await connectToDatabase();
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", Authrouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");

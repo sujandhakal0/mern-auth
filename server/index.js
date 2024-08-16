cf26c7faa22acc7770be4d8c1731e3a566c52753
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-16 16:16:18 +0545

    create test api route

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./config/database.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 await connectToDatabase();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");

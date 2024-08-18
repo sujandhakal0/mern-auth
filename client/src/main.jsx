dc182a9e1613b252a0c67c97c06588ab1d63e54d
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-18 18:24:17 +0545

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-    <Toaster />
+    {/* <PersistGate loading={<div>Loading...</div>} persistor={persistor}> */}
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+      <Toaster />
+    </PersistGate>
   </Provider>
 );

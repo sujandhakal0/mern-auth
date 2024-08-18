a635cd8e3e6aa053c5592c248e18b6e2110a1d39
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-18 20:33:32 +0545

    update header for authentication

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
-
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
-       
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Mern Auth
             </span>
           </Link>
           <div className="flex items-center lg:order-2">
-            <Link
-              to="/signin"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >
-              Log in
-            </Link>
+            {currentUser ? (
+              <Link to="/profile">
+                {" "}
+                <img
+                  src={currentUser.profilePhoto}
+                  alt="profile"
+                  className="h-11 w-11 rounded-full object-cover"
+                />
+              </Link>
+            ) : (
+              <Link
+                to="/signin"
+                className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              >
+                Log in
+              </Link>
+            )}
 
             <button
               data-collapse-toggle="mobile-menu-2"

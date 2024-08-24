802d0be9ebd245902efab7593551bf9a23252097
Sujan Dhakal <101464442+sujandhakal0@users.noreply.github.com>
2024-08-24 12:08:55 +0545

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mern-auth-backend-27h1.onrender.com",
         // changeOrigin: true,
         secure: false,
       },

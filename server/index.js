4b0ff678c54ed84d350d2b8bea6c856052445f1e
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-26 20:58:18 +0545

    fixed package.json

--- server/index.js
+++ server/index.js
@@ -4,17 +4,16 @@ import dotenv from "dotenv";
 import { connectToDatabase } from "./config/database.js";
 import userRouter from "./routes/user.route.js";
 import Authrouter from "./routes/auth.route.js";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from 'path';
+import path from "path";
 
 const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use(express.json());

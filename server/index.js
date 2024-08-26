add1721b065e9b111cc9a717d3599c4510539ed1
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-26 20:49:02 +0545

    add pre production changed

--- server/index.js
+++ server/index.js
@@ -6,8 +6,16 @@ import userRouter from "./routes/user.route.js";
 import Authrouter from "./routes/auth.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,15 +24,15 @@ app.use(cookieParser());
 dotenv.config({ path: "./config/config.env" });
 await connectToDatabase();
 
-const allowedOrigins = ["https://mern-auth-ebon-sigma.vercel.app/"];
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// const allowedOrigins = ["https://mern-auth-ebon-sigma.vercel.app/"];
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     methods: "*",
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", Authrouter);
